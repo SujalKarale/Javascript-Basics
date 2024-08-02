f9023ccd7c508082384bfc1d44589d80a1d39b00
Sujal Karale <150381280+SujalKarale@users.noreply.github.com>
2024-08-03 00:34:26 +0530

    Add files via upload

---
+++ foreachloop.js
@@ -0,0 +1,12 @@
+arr = [4,"sujal",7.9,true,[4,7,4,8]]
+// For loop
+// for(let i=0;i<arr.length;i++){
+//     console.log(i,arr[i]);
+// }
+
+// ForOf loop
+for (const ele of arr) {
+    //element mein change ho raha hai pr array pr nahi
+    console.log(ele);
+    //making copy 
+}
